e2d36e2c3959491b2501406ac0ae13e8a6b55d30
Vivek Garg <vkg1617@gmail.com>
2024-01-09 22:43:56 +0530

    first-commit

---
+++ es6/3_function.js
@@ -0,0 +1,106 @@
+// console.log(this);
+
+
+//called in object
+// const obj = {
+//     name:"vivek",
+//     age:20,
+//     myFun: function(){
+//         console.log(this);
+//     },
+//     // myFun2: ()=>{
+//     //     console.log(this.name)
+//     // }
+
+// }
+
+// obj.myFun();
+// // obj.myFun2();
+
+
+//normally called in outer 
+// function vivek1(){
+//     this.name = "vivek"
+//     console.log(this.name);
+// }
+// vivek1()
+
+
+
+//intanse examples:
+// function vivek2(){
+//     this.name = "vivek"
+//     this.myFun =  function(){
+//         console.log(this.name);
+//     }
+// }
+
+// const normalFunction  = new vivek2();
+// normalFunction.myFun();
+// normalFunction.name = "abc";
+// normalFunction.myFun()
+
+
+// const arrow = ()=>{
+//     console.log(this);
+// }
+
+
+
+//**arrow functions = >
+
+// // Regular function as a method of an object
+// const obj1 = {
+//     name: 'Object 1',
+//     regularFunction: function () {
+//         console.log("Regular function:", this.name);
+//     },
+// };
+
+// // Arrow function as a method of an object
+// const obj2 = {
+//     name: 'Object 2',
+//     arrowFunction: () => {
+//         console.log("Arrow function:", obj2.name);
+//     },
+// };
+
+// // obj1.regularFunction();
+// // obj2.arrowFunction();
+
+
+// class Button {
+//    constructor(){
+//     this.name = "Button";
+//    }
+//     regularFunction () {
+//         console.log(this)
+//     }
+
+//     arrowFunction = () =>{
+//         console.log(this)
+//     }
+// }
+
+// const button = new Button();
+
+// button.regularFunction();
+// button.arrowFunction();
+
+// const obj = {
+//     fun1:button.regularFunction,
+//     fun2:button.arrowFunction
+// }
+
+// obj.fun1()
+// obj.fun2()
+
+console.log(regularFunction())
+function regularFunction(){
+    this.name = "vivek";
+}
+
+// console.log(arrowFunction)
+// const arrowFunction = () =>{
+
+// }
---
+++ es6/4_variables.js
@@ -0,0 +1,22 @@
+// var x  = "abc";
+
+// console.log(x);
+
+// function test(){
+//     var y  = "abcd";
+//     console.log(y);
+// }
+
+// test();
+
+// let y = 1;
+// for (let i = 0; i <10; i++){
+//     console.log(y);
+// }
+// console.log(y)
+
+// const y = -1;
+// for (let i = 0; i <10; i++){
+//     y=i;
+//     console.log(y);
+// }
